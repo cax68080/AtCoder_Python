859ac303907f15a9eda0586b089b87ebe66e071f
cax68080 <cax68080@pop06.odn.ne.jp>
2023-12-24 11:36:28 +0900

    2023/12/24

---
+++ 2023/ABC334_20231224/contest_b.py
@@ -0,0 +1,37 @@
+from sys import stdin
+readline = stdin.readline
+a,m,l,r = map(int,readline().rstrip("\n").split())
+count = 0
+pos = a
+if a < l:
+    while True:
+        pos += m
+        if (l <= pos) and (pos <= r):
+            count += 1
+        elif r < pos:
+            break
+    print(count)
+if (a >= l) and (a < r):
+    while True:
+        pos += m
+        if (l <= pos) and (pos <= r):
+            count += 1
+        elif r < pos:
+            break
+    while True:
+        pos -= m
+        if (l <= pos) and (pos <= r):
+            count += 1
+        elif l > pos:
+            break
+    print(count)
+if pos > r:
+    while True:
+        pos -= m
+        if (l <= pos) and (pos <= r):
+            count += 1
+        elif l > pos:
+            break
+    print(count)
+
+    
